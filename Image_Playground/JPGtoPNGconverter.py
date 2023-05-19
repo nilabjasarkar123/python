149ecdb676a4f9ae7919117a5f293fad1426a109
nilabjasarkar123 <nil.sarkar002@gmail.com>
2023-05-19 09:48:02 +0530

    first commit

---
+++ Image_Playground/JPGtoPNGconverter.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from PIL import Image
+
+#grab first and second argument
+
+#check is new\ exists, if not create
+
+#loop through pokedex
+#convert img to png
+#save to the new folder
