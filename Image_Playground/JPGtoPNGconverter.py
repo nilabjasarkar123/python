4fc2dbda2de5d9d08385c24f44f087750a586a74
nilabjasarkar123 <nil.sarkar002@gmail.com>
2023-05-19 14:57:46 +0530

    second commit

--- Image_Playground/JPGtoPNGconverter.py
+++ Image_Playground/JPGtoPNGconverter.py
@@ -1,11 +1,24 @@
 import sys
 import os
 from PIL import Image
+from os import listdir
 
 #grab first and second argument
 
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+#print(input_path, output_path) # command : python JPGtoPNGconverter.py pokedesk new
 #check is new\ exists, if not create
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
 
 #loop through pokedex
+for filename in os.listdir(input_path):
+    img = Image.open(f'{input_path}\{filename}')
+    img.save(f'{output_path}\{filename}.png','png')
+    print('all done')
+
 #convert img to png
 #save to the new folder
